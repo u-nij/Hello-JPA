d9f76b3e6821f20a55ed822ecfd7fa0b9b1a7680
u-nij <u.nij0509@gmail.com>
2022-05-06 00:18:36 +0900

    엔티티 매핑

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -11,96 +11,131 @@ public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 
-
-        // ===================== JPA =====================
-
-        EntityManager em = emf.createEntityManager();
-        EntityTransaction tx = em.getTransaction();
-        tx.begin();
-
-        // code
-        try {
-//            Member member = new Member();
 //
-//            // 생성
-//            member.setId(1L);
-//            member.setName("HelloA");
+//        // ===================== JPA =====================
+//
+//        EntityManager em = emf.createEntityManager();
+//        EntityTransaction tx = em.getTransaction();
+//        tx.begin();
+//
+//        // code
+//        try {
+////            Member member = new Member();
+////
+////            // 생성
+////            member.setId(1L);
+////            member.setName("HelloA");
+//
+//            Member findMember = em.find(Member.class, 1L);
+//
+//            // 조회
+//            System.out.println("findMember.id = " + findMember.getId());
+//            System.out.println("findMember.name = " + findMember.getName());
+//
+//            // 수정
+//            findMember.setName("HelloJPA");
+//
+//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+//                    .setFirstResult(5)
+//                    .setMaxResults(8)
+//                    .getResultList();
+//
+//            for (Member member : result) {
+//                System.out.println("member = " + member.getName());
+//            }
+//
+//            tx.commit();
+//
+//        } catch (Exception e) {
+//            tx.rollback();
+//        } finally {
+//            em.close();
+//        }
+//
+//
+//        // =========== 영속성 매핑 (Persistence Mapping) ===========
+//
+//        EntityManager emPM = emf.createEntityManager();
+//        EntityTransaction txPM = emPM.getTransaction();
+//        txPM.begin();
+//
+//        try {
+//            // 비영속
+//            Member member = new Member(101L, "HelloJPA");
+//
+//            // 영속
+//            System.out.println("===== BEFORE =====");
+//            emPM.persist(member);
+//            System.out.println("===== AFTER =====");
+//
+//            Member findMember = emPM.find(Member.class, 101L);
+//            System.out.println("findMember.id = " + findMember.getId());
+//            System.out.println("findMember.Name = " + findMember.getName());
+//
+//            Member a = emPM.find(Member.class, 101L);
+//            Member b = emPM.find(Member.class, 101L);
+//            System.out.println(a == b); // 동일성 비교
+//
+//            // 변경 감지
+//            Member member1 = emPM.find(Member.class, 150L);
+//            member1.setName("AAAAA");
+//            emPM.persist(member1);
+//
+//            System.out.println("========================");
+//
+//            Member member2 = emPM.find(Member.class, 151L);
+//            member2.setName("BBBBB");
+//
+//            Member member200 = new Member(200L, "member200");
+//            emPM.persist(member200);
+//            emPM.flush();
+//            System.out.println("========================");
+//
+//            // 준영속
+//            Member find200 = emPM.find(Member.class, 200L);
+//            find200.setName("AAAAA");
+//            emPM.detach(find200); // AAAAA로 변경되지 않음
+//
+//            txPM.commit();
+//        } catch (Exception e) {
+//            txPM.rollback();
+//        } finally {
+//            emPM.close();
+//        }
 
-            Member findMember = em.find(Member.class, 1L);
 
-            // 조회
-            System.out.println("findMember.id = " + findMember.getId());
-            System.out.println("findMember.name = " + findMember.getName());
+        // =========== 엔티티 매핑 (Entity Mapping) ===========
 
-            // 수정
-            findMember.setName("HelloJPA");
-
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(5)
-                    .setMaxResults(8)
-                    .getResultList();
-
-            for (Member member : result) {
-                System.out.println("member = " + member.getName());
-            }
-
-            tx.commit();
-
-        } catch (Exception e) {
-            tx.rollback();
-        } finally {
-            em.close();
-        }
-
-
-        // =========== 영속성 매핑 (Persistence Mapping) ===========
-
-        EntityManager emPM = emf.createEntityManager();
-        EntityTransaction txPM = emPM.getTransaction();
-        txPM.begin();
+        EntityManager emEM = emf.createEntityManager();
+        EntityTransaction txEM = emEM.getTransaction();
+        txEM.begin();
 
         try {
-            // 비영속
-            Member member = new Member(101L, "HelloJPA");
+            Member member1 = new Member();
+            member1.setUsername("A");
+            Member member2 = new Member();
+            member2.setUsername("B");
+            Member member3 = new Member();
+            member3.setUsername("C");
 
-            // 영속
-            System.out.println("===== BEFORE =====");
-            emPM.persist(member);
-            System.out.println("===== AFTER =====");
+            System.out.println("============================");
 
-            Member findMember = emPM.find(Member.class, 101L);
-            System.out.println("findMember.id = " + findMember.getId());
-            System.out.println("findMember.Name = " + findMember.getName());
+                                    // DB MEMBER_SEQ : -49
+            emEM.persist(member1);    // DB MEMBER_SEQ : 1->51(2번 호출)  |  APP 1
+            emEM.persist(member2);    // APP 2 (DB 부르지 않고, MEM에서 호출)
+            emEM.persist(member3);    // APP 3 (DB 부르지 않고, MEM에서 호출)
 
-            Member a = emPM.find(Member.class, 101L);
-            Member b = emPM.find(Member.class, 101L);
-            System.out.println(a == b); // 동일성 비교
+            System.out.println("member.id = " + member1.getId());
+            System.out.println("member.id = " + member2.getId());
+            System.out.println("member.id = " + member3.getId());
 
-            // 변경 감지
-            Member member1 = emPM.find(Member.class, 150L);
-            member1.setName("AAAAA");
-            emPM.persist(member1);
+            System.out.println("============================");
 
-            System.out.println("========================");
-
-            Member member2 = emPM.find(Member.class, 151L);
-            member2.setName("BBBBB");
-
-            Member member200 = new Member(200L, "member200");
-            emPM.persist(member200);
-            emPM.flush();
-            System.out.println("========================");
-
-            // 준영속
-            Member find200 = emPM.find(Member.class, 200L);
-            find200.setName("AAAAA");
-            emPM.detach(find200); // AAAAA로 변경되지 않음
-
-            txPM.commit();
+            txEM.commit();
         } catch (Exception e) {
-            txPM.rollback();
+            txEM.rollback();
         } finally {
-            emPM.close();
+            emEM.close();
         }
 
         emf.close();
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,21 +1,32 @@
 package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity
+@SequenceGenerator( // SEQUENCE 전략
+        name = "MEMBER_SEQ_GENERATOR",
+        sequenceName = "MEMBER_SEQ", //매핑할 데이터베이스 시퀀스 이름
+        initialValue = 1, allocationSize = 50)
+//@TableGenerator(    // TABLE 전략
+//        name = "MEMBER_SEQ_GENERATOR",
+//        table = "MY_SEQUENCES", // 테이블 이름
+//        pkColumnValue = "MEMBER_SEQ", allocationSize = 1)
 public class Member {
 
+
     @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY) // IDENTITY 전략
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, // SEQUENCE 전략
+                    generator = "MEMBER_SEQ_GENERATOR")
+//    @GeneratedValue(strategy = GenerationType.TABLE, // TABLE 전략
+//                    generator = "MEMBER_SEQ_GENERATOR")
     private Long id;
-    private String name;
 
-    public Member() {}
-
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
+    @Column(name = "name", nullable = false)
+    private String username;
 
     public Long getId() {
         return id;
@@ -25,11 +36,31 @@ public class Member {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
     }
+
+//    private Integer age;
+//
+//    @Enumerated
+//    private RoleType roleType;
+//
+//    @Temporal(TemporalType.TIMESTAMP)
+//    private Date createdDate;
+//
+//    @Temporal(TemporalType.TIMESTAMP)
+//    private Date lastModifiedDate;
+//
+//    private LocalDate testLocalDate;
+//    private LocalDateTime testLocalDateTime;
+//
+//    @Lob
+//    private String description;
+
+    public Member() {}
+
 }
